a5e454e7cc305b5f8aeefc4a490f12b7f80176cf
schontz0310 <schontz0310@gmail.com>
2020-07-15 04:01:48 -0300

    initial commit

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <View>
+          <Text>SignUp</Text>
+        </View>
+      </Container>
+    </>
+  );
+};
+
+export default SignUp;
